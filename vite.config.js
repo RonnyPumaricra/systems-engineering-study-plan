ca4bd92522e7cbdc98038fcf066c77fda9ea1575
RonnyPumaricra <ronnylpqwerty@gmail.com>
2024-01-17 21:59:06 -0500

    Added preview and gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = true;
+// const folderName = path.basename(process.cwd()) + "/";
+const folderName = "systems-engineering-study-plan";
+const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
+  mode,
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
 })
